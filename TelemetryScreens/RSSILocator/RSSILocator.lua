6c7550eea57d25087f900dc7394c3c68cffebfc5
Bauerpauer <bauer.mail@gmail.com>
2015-06-21 17:49:23 -0500

    Adds "RSSI Locator" Telemetry Screen.

---
+++ TelemetryScreens/RSSILocator/RSSILocator.lua
@@ -0,0 +1,31 @@
+-- RSSI Strength Model Locator
+-- by Scott Bauer 6/29/2014
+--
+-- Copy this into a SCRIPTS/<modelname>/ with the name `telem#.lua`,
+-- where # is telemetry screen number you'd like to use.  Drop
+-- `sbeep.wav` into the `SOUNDS/` folder.
+--
+-- When you inevitably drop your quad into tall grass, long-press
+-- "Page" until you enter Telemetry Mode.  Press "Page" until you
+-- continious short beeps.  As you get closer to your model the
+-- beeps will become more frequent, hopefully leading you to the
+-- crash site.
+
+local delayMillis = 100
+local nextPlayTime = getTime()
+
+local function run(event)
+  -- Title
+  lcd.drawText(3, 3, "RSSI Model Locator", 0)
+
+  -- RSSI # and Label
+  lcd.drawNumber(210, 10, getValue("rssi"), XXLSIZE)
+  lcd.drawText(187, 54, "RSSI", 0)
+
+  if getTime() >= nextPlayTime then
+    playFile("/SOUNDS/sbeep.wav")
+    nextPlayTime = getTime() + delayMillis - getValue("rssi")
+  end
+end
+
+return { run = run }
