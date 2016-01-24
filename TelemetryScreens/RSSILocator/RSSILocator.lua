f2c95c929167ced5dbacd573b1bc89ba40c164b0
Noys <albertitomsn@gmail.com>
2016-01-24 01:08:10 +0100

    Update RSSILocator.lua
    Changed getValue source to make it work with OpenTX 2.1.7.

--- TelemetryScreens/RSSILocator/RSSILocator.lua
+++ TelemetryScreens/RSSILocator/RSSILocator.lua
@@ -1,9 +1,12 @@
 -- RSSI Model Locator
 -- by Scott Bauer 6/21/2015
 --
--- Copy this into a SCRIPTS/<modelname>/ with the name `telem#.lua`,
--- where # is telemetry screen number you'd like to use.  Drop
+-- Copy this into a SCRIPTS/TELEMETRY/ with the name `LM###.lua`,
+-- where ### is a number used to identify the script later on. (i.e. LM100.lua  Drop
 -- `sbeep.wav` into the `SOUNDS/` folder.
+--
+-- In your radio, go into the Telemetry page and in the screen you want to use select
+-- "Script" and the script with the name you just wrote.
 --
 -- When you inevitably drop your quad into tall grass, long-press
 -- "Page" until you enter Telemetry Mode.  Press "Page" until you
@@ -19,12 +22,12 @@ local function run(event)
   lcd.drawText(3, 3, "RSSI Model Locator", 0)
 
   -- RSSI # and Label
-  lcd.drawNumber(210, 10, getValue("rssi"), XXLSIZE)
+  lcd.drawNumber(210, 10, getValue(202), XXLSIZE)
   lcd.drawText(187, 54, "RSSI", 0)
 
   if getTime() >= nextPlayTime then
     playFile("/SOUNDS/sbeep.wav")
-    nextPlayTime = getTime() + delayMillis - getValue("rssi")
+    nextPlayTime = getTime() + delayMillis - getValue(202)
   end
 end
 
